67eb3fbd04b030d63aa4b1e23201c2efa3ad5988
AleG1312 <aguerrarod2003@gmail.com>
2024-10-13 12:48:50 -0600

    Documentación Interna lista de la clase CuatroEnLinea y corrección de errores menores. Ganador en diagonales pendiente.

--- Proyecto1/CuatroEnLinea.java
+++ Proyecto1/CuatroEnLinea.java
@@ -1,18 +1,24 @@
-
 /**
- * Write a description of class CuatroEnLinea here.
+ * CuatroEnLinea
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Luis Guido, Alejandro Guerra
+ * @version 17-10-2024
  */
 public class CuatroEnLinea
 {
-    // instance variables - replace the example below with your own
+    //Matriz que representa el tablero
     private String[][] tablero;
+    //String que representa el jugador
     private String jugadorActual;
-    
-    /**
-     * Método Constructor
+    /*
+     * Método Constructor:
+     *      Crea mi objeto CuatroEnLinea.
+     * Parámetros de entrada:
+     *      Ninguno.
+     * Parámetros de salida:
+     *      Ninguno.
+     * Respuesta Esperada:
+     *      Inicializa el tablero y el símbolo del jugador actual.
      */
     public CuatroEnLinea(){
         this.tablero = new String[6][7];
@@ -21,14 +27,40 @@ public class CuatroEnLinea
     public String[][] getTablero(){
         return this.tablero;
     }
-    public boolean movimientoValido(int columna){
-        boolean movimientoValido = true;
-        if(this.tablero[0][columna] != null){
-            cambiarJugador();
-            movimientoValido = false;
-        }
-        return movimientoValido;
+    public void setTablero(String[][] tablero){
+        this.tablero = tablero;
     }
+    public String getJugadorActual(){
+        return this.jugadorActual;
+    }
+    public void setJugadorActual(String jugadorActual){
+        this.jugadorActual = jugadorActual;
+    }
+    /*
+     * Método columnaLlena:
+     *      Revisa si para una columna dada todavía hay campo o no.
+     * Parámetros de entrada:
+     *      Entero representando una columna.
+     * Parámetros de salida:
+     *      Booleano indicando true si está llena y false si no lo está.
+     * Respuesta Esperada:
+     *      Comprueba si hay espacio revisando la primera fila en la columna indicada. De estar vacía, es que queda campo. 
+     *      De lo contrario, no.
+     */
+    public boolean columnaLlena(int columna){
+        boolean columnaLlena = (this.tablero[0][columna] == null)? false:true;
+        return columnaLlena;
+    }
+    /*
+     * Método hacerMovimiento:
+     *      Llena una casilla del tablero con el símbolo del jugador actual.
+     * Parámetros de entrada:
+     *      Entero representando una columna.
+     * Parámetros de salida:
+     *      Ninguno.
+     * Respuesta Esperada:
+     *      En la columna indicada, revisa la última casilla no nula y la rellena con el símbolo del jugador actual.
+     */
     public void hacerMovimiento(int columna){
         for (int fila = this.tablero.length-1; fila >= 0; fila --){
             if (this.tablero[fila][columna] == null){
@@ -37,44 +69,64 @@ public class CuatroEnLinea
             }            
         }
     }
+    /*
+     * Método mostrarTablero:
+     *      Imprime el estado actual del tablero en pantalla.
+     * Parámetros de entrada:
+     *      Ninguno.
+     * Parámetros de salida:
+     *      Ninguno.
+     * Respuesta Esperada:
+     *      Recorre el array en filas y luego columnas. Concatena en un String la información de cada columna junto con otros elementos (para ayudar con el formato)
+     *      y posteriormente imprime la fila en pantala.
+     */
     public void mostrarTablero(){
         String primeraFila;
         for (String[] fila : tablero){
             primeraFila = "|";
             for(String elemento : fila){
-                if (elemento == null){
-                    primeraFila += " ";
-                }
-                else{
-                    primeraFila += elemento;
-                }
+                primeraFila += (elemento == null)? " " : elemento;
                 primeraFila += "|";
             }
             System.out.println(primeraFila); 
         }
     }
+    /*
+     * Método ganadorFila:
+     *      Identifica si hay ganador en alguna fila.
+     * Parámetros de entrada:
+     *      Ninguno.
+     * Parámetros de salida:
+     *      String representando el ganador de fila. Puede regresar "null" si no hay ganador, o el símbolo del ganador en caso de que haya uno.
+     * Respuesta Esperada:
+     *      Empezando en la fila inferior, revisa si cualesquiera 4 espacios consecutivos (tomando como primer elemento, el elemento situado más a la izquierda)
+     *      tienen un mismo símbolo, indicando así si hay ganador en esos espacios. El programa continúa hasta que identifique un ganador o haya revisado todo 
+     *      el tablero sin encontrar nada.
+     */
     public String ganadorFila(){
         String ganador = null;
+        //columnaMaximaDeRevision corresponde a la última columna de cualquier fila en la que debe revisar si en los 3 espacios siguientes a la derecha
+        //puede haber un ganador o no. Si hay 7 columnas, hay un posible ganador de fila hasta la columna 4.
+        int columnaMaximaDeRevision = this.tablero[0].length-4;
         //Revisión de filas:
-        for (int i = 5; i >= 0; i--){
+        for (int i = this.tablero.length-1; i >= 0; i--){
             //Voy a revisar desde el elemento inicial de la fila, hasta el elemento máximo de revisión de la misma fila
-            for(int j = 0; j <= 3; j++){
-                //Defino el ganador como el primer elemento que estoy revisando en la fila
+            for(int j = 0; j <= columnaMaximaDeRevision; j++){
+                //Defino el posible ganador como el primer elemento que estoy revisando en la fila
                 ganador = this.tablero[i][j];
                 //Solo hago la revisión si el elemento que estoy revisando no está vacío
                 if(ganador != null){
-                    //Para el elemento en el que estoy, reviso los cuatro elementos posteriores incluyendo en el que estoy
-                    for(int elemento = j; elemento <= (j+3); elemento++){
+                    //Reviso si los tres elementos siguientes son iguales a mi posible ganador
+                    for(int elemento = j+1; elemento <= (j+3); elemento++){
                         //El valor de comparación es uno de los 3 elementos posteriores a mi valor inicial
                         String valorDeComparacion = this.tablero[i][elemento];
-                        //Si es diferente, finalizo la comparación del primero intento de  en línea de la fila
+                        //Si el valor de comparación es diferente a mi posible ganador, finalizo la búsqueda de ganador para esos 4 elementos y reestablezco el valor de ganador en null
                         if (valorDeComparacion != ganador){
                             ganador = null;
                             break;
                         }                    
                     }
                 }
-                
                 //Si encontró un ganador en la fila, devuelvo el valor, sino paso al siguiente valor de comparación de la misma fila
                 if (ganador != null){
                     return ganador;
@@ -83,21 +135,36 @@ public class CuatroEnLinea
         }
         return ganador;        
     }
+    /*
+     * Método ganadorColumna:
+     *      Identifica si hay ganador en alguna columna.
+     * Parámetros de entrada:
+     *      Ninguno.
+     * Parámetros de salida:
+     *      String representando el ganador de la columna. Puede regresar "null" si no hay ganador, o el símbolo del ganador en caso de que haya uno.
+     * Respuesta Esperada:
+     *      Empezando en la fila inferior, revisa si cualesquiera 4 espacios consecutivos (tomando como primer elemento, el elemento más abajo)
+     *      tienen un mismo símbolo, indicando así si hay ganador en esos espacios. El programa continúa hasta que identifique un ganador o haya revisado todo 
+     *      el tablero sin encontrar nada.
+     */
     public String ganadorColumna(){
         String ganador = null;
+        //filaMaximaDeRevision corresponde a la última fila de cualquier columna en la que debe revisar si en los 3 espacios siguientes hacia arriba
+        //puede haber un ganador o no. Si hay 6 filas, hay un posible ganador de columna hasta la columna 3.
+        int filaMaximaDeRevision = 3;
         //Revisión de columnas:
-        for (int j = 0; j <= 6; j++){
-            //Voy a revisar desde el último elemento de la fila, hasta la tercera posición desde el suelo
-            for(int i = 5; i >= 3; i--){
-                //Defino el ganador como el primer elemento que estoy revisando en la columna
+        for (int j = 0; j <= (this.tablero[0].length-1); j++){
+            //Voy a revisar desde el último elemento de la columna, hasta el elemento máximo de revisión de la misma columna
+            for(int i = (this.tablero.length-1); i >= filaMaximaDeRevision; i--){
+                //Defino el posible ganador como el primer elemento que estoy revisando en la fila
                 ganador = this.tablero[i][j];
                 //Solo hago la revisión si el elemento que estoy revisando no está vacío
                 if(ganador != null){
+                    //Reviso si los tres elementos de arriba son iguales a mi posible ganador
                     for(int elemento = (i-1); elemento >= (i-3); elemento--){
-                        //System.out.println("Estoy comparando la posición [" + i + "," + j + "], con la posición [" + elemento + "," + j + "]");
                         //El valor de comparación es uno de los 3 elementos superiores a mi valor inicial
                         String valorDeComparacion = this.tablero[elemento][j];
-                        //Si es diferente, finalizo la comparación del primero intento de  en línea de la fila
+                        //Si el valor de comparación es diferente a mi posible ganador, finalizo la búsqueda de ganador para esos 4 elementos y reestablezco el valor de ganador en null
                         if (valorDeComparacion != ganador){
                             ganador = null;
                             break;
@@ -112,6 +179,17 @@ public class CuatroEnLinea
         }
         return ganador;
     }
+    /*
+     * Método esGanador:
+     *      Revisa si hay un ganador en la partida.
+     * Parámetros de entrada:
+     *      Ninguno.
+     * Parámetros de salida:
+     *      String representando el ganador de la partida. Puede regresar "null" si no hay ganador, o el símbolo del ganador en caso de que haya uno.
+     * Respuesta Esperada:
+     *      Llamará a las funciones de comprobación de ganador en las filas, columnas y diagonales respectivamente. El programa se detiene cuando 
+     *      encuentre un ganador o cuando haya ejecutado las funciones mencionadas sin encontrar nada.
+     */
     public String esGanador(){
         String ganador = null;
         //Reviso si hay ganador en las filas
@@ -129,6 +207,40 @@ public class CuatroEnLinea
         //En el caso de que no haya ganador en filas/columnas/diagonales, retorna el valor de ganador = null
         return ganador;
     }
+    /*
+     * Método esTableroLleno:
+     *      Revisa si el tablero se encuentra lleno.
+     * Parámetros de entrada:
+     *      Ninguno.
+     * Parámetros de salida:
+     *      Booleano que representa si el tablero está lleno.
+     * Respuesta Esperada:
+     *      Revisa únicamente los espacios de la primera fila. Si hay alguno libre, el tablero no está lleno.
+     */
+    public boolean esTableroLleno(){
+        //Asumo que el tablero está lleno
+        boolean tableroLleno = true;
+        //Reviso la primera fila
+        for(int j = 0; j <= (this.tablero[0].length-1);j++){
+            //Si el elemento está vacío, el tablero no está lleno
+            if(this.tablero[0][j] == null){
+                tableroLleno = false;
+                return tableroLleno;
+            }
+        }
+        return tableroLleno;
+    }
+    /*
+     * Método esEmpate:
+     *      Me indica si hay un empate.
+     * Parámetros de entrada:
+     *      String representando el ganador (null/algún jugador).
+     * Parámetros de salida:
+     *      Booleano representando si hay empate o no.
+     * Respuesta Esperada:
+     *      Si hay un ganador, entonces no hay un empate. 
+     *      Si no hay un ganador y el tablero está lleno, entonces hay empate.
+     */
     public boolean esEmpate(String ganador){
         boolean empate = false;
         if(ganador == null && esTableroLleno() == true){
@@ -136,31 +248,44 @@ public class CuatroEnLinea
         }
         return empate;
     }
-    public boolean esTableroLleno(){
-        boolean tableroLleno = true;
-        for(String[] fila : tablero){
-            for (String elemento: fila){
-                if(elemento == null){
-                    tableroLleno = false;
-                    return tableroLleno;
-                }
-            }
-        }
-        return tableroLleno;
-    }
+
+    /*
+     * Método esJuegoTerminado:
+     *      Revisa si el juego terminó.
+     * Parámetros de entrada:
+     *      String representando el ganador del juego.
+     *      Booleano representando el empate.
+     * Parámetros de salida:
+     *      Booleano representando si el juego terminó.
+     * Respuesta Esperada:
+     *      Si hay ganador, el juego terminó.
+     *      Si hay empate, el juego terminó.
+     *      Si no se cumplen, el juego no ha terminado.
+     */
     public boolean esJuegoTerminado(String ganador, boolean esEmpate){
         boolean juegoTerminado = false;
         if(ganador != null){
                 juegoTerminado = true;
         }
         else{
-            if(ganador == null && esEmpate == true){
+            if(esEmpate == true){
                 juegoTerminado = true;
             }
         }
         return juegoTerminado;
     }
-    public void resultadosFinDelJuego(String ganador, boolean esEmpate){
+    /*
+     * Método resultadosFinales:
+     *      Imprime los resultados del fin de la partida.
+     * Parámetros de entrada:
+     *      String representando el ganador del juego.
+     *      Booleano representando el empate.
+     * Parámetros de salida:
+     *      Ninguno.
+     * Respuesta Esperada:
+     *      Imprime en pantalla ya sea el ganador, o el mensaje de que el juego concluyó en empate.
+     */
+    public void resultadosFinales(String ganador, boolean esEmpate){
         if(ganador != null){
             System.out.println("El ganador es: " + ganador);
         }
@@ -168,6 +293,16 @@ public class CuatroEnLinea
             System.out.println("El juego ha terminado en empate");
         }
     }
+    /*
+     * Método cambiarJugador:
+     *      Cambia el símbolo del jugador actual
+     * Parámetros de entrada:
+     *      Ninguno.
+     * Parámetros de salida:
+     *      Ninguno.
+     * Respuesta Esperada:
+     *      Revisa el símbolo del jugador actual y lo cambia entre las dos posibles opciones.
+     */
     public void cambiarJugador(){
         if(this.jugadorActual == "1"){
             this.jugadorActual = "2";
--- Proyecto1/Pruebas.java
+++ Proyecto1/Pruebas.java
@@ -9,12 +9,12 @@ import java.io.IOException;
  * @version (a version number or a date)
  */
 public class Pruebas{   
-    public static void mostrarMenu(){
+    private static void mostrarMenu(){
         System.out.println("Seleccione el juego que desea jugar: ");
         System.out.println("1. CuatroEnLinea");
         System.out.println("2. Salir");
     }
-    public static int seleccionarJuego(){
+    private static int seleccionarJuego(){
         Scanner input = new Scanner(System.in);
         int juego;
         do{
@@ -22,25 +22,27 @@ public class Pruebas{
         }while(juego != 1 && juego != 2);
         return juego;
     }
-    public static void jugar(int juego){
+    private static void jugar(int juego){
         if (juego == 1){
             Scanner input = new Scanner(System.in);
             CuatroEnLinea nuevoJuego = new CuatroEnLinea();
-    
+            
             boolean juegoTerminado = false;
             while(juegoTerminado == false){
                 nuevoJuego.mostrarTablero();
                 
                 System.out.println("Ingrese una columna para hacer su movimiento");
                 int columna = input.nextInt(); 
-                boolean movimientoValido = nuevoJuego.movimientoValido(columna);
-                if (movimientoValido == false){
+                
+                //Me verifica que la columna no esté llena
+                boolean columnaLlena = nuevoJuego.columnaLlena(columna);
+                if (columnaLlena == true){
                     do{
                         nuevoJuego.mostrarTablero();
-                        System.out.println("Ingrese una columna válida para hacer su movimiento");
+                        System.out.println("La columna: " + columna + " está llena. Ingrese una diferente para hacer su movimiento");
                         columna = input.nextInt(); 
-                        movimientoValido = nuevoJuego.movimientoValido(columna);
-                    }while(movimientoValido == false);
+                        columnaLlena = nuevoJuego.columnaLlena(columna);
+                    }while(columnaLlena == true);
                 }
                 nuevoJuego.hacerMovimiento(columna);
                 
@@ -57,7 +59,7 @@ public class Pruebas{
                 //Protocolo de fin del juego
                 else{
                     nuevoJuego.mostrarTablero();
-                    nuevoJuego.resultadosFinDelJuego(ganador, empate);
+                    nuevoJuego.resultadosFinales(ganador, empate);
                 }
             }
         }
@@ -72,5 +74,33 @@ public class Pruebas{
             }
         }while(juego != 2);
         System.out.println("Saliendo de la simulación");
+    }
+    public static void main2(){
+        CuatroEnLinea juego = new CuatroEnLinea();
+        juego.mostrarTablero();
+        String[][] tableroEmpate = matrizEmpate();
+        juego.setTablero(tableroEmpate);
+        //Compruebo si hay un ganador
+        String ganador = juego.esGanador();
+        //Compruebo si hay un empate
+        boolean empate = juego.esEmpate(ganador);
+        //Compruebo el fin del juego
+        boolean juegoTerminado = juego.esJuegoTerminado(ganador, empate);
+        if(juegoTerminado == true){
+            juego.mostrarTablero();
+            juego.resultadosFinales(ganador, empate);
+        }
+    }
+    private static String[][] matrizEmpate(){
+        int alternador = 1;
+        String[][] tablero = new String[6][7];
+        for(int i = 5; i >= 0; i--){
+            for(int j = 0; j <= 6; j++){
+                String elemento = (alternador == 1)? "1":"2";
+                tablero[i][j] = elemento;
+                alternador *= -1;
+            }
+        }
+        return tablero;
     }
 }
